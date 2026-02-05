74af121eef26ab0fc4b634a2f01e051e0602246f
boevDev <hg.hellstaff@gmail.com>
2026-02-06 02:04:12 +0500

    chore: configuration monorepo

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -3,7 +3,7 @@ import type { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  await app.listen(process.env.PORT || 3000);
+	const app = await NestFactory.create<NestExpressApplication>(AppModule);
+	await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
