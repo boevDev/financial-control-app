e81fda2626309255d627ee5001c1c6683c7d38dd
boevdev <arrboy03@gmail.com>
2026-06-22 20:15:15 +0500

    chore: setup port and cors for railway

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,13 +6,17 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	app.enableCors({
-		origin: 'http://localhost:5173',
+		origin: ['http://localhost:5173', 'https://financial-control-app-six.vercel.app/'],
 		credentials: true,
 		allowedHeaders: ['Content-Type', 'Authorization'],
 	});
 
 	app.use(cookieParser());
 
-	await app.listen(3000);
+	const port = process.env.PORT || 3000;
+
+	await app.listen(port, '0.0.0.0');
+
+	console.log(`Backend is running on port: ${port}`);
 }
 bootstrap();
